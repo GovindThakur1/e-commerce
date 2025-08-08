d02c2d32aa2ad6f6ac88f1943e578cb230f15493
GovindThakur1 <107384578+GovindThakur1@users.noreply.github.com>
2025-08-08 11:52:52 +0545

    worked on user setvice

--- src/main/java/com/govind/ecommerce/service/user/IUserService.java
+++ src/main/java/com/govind/ecommerce/service/user/IUserService.java
@@ -1,9 +1,17 @@
 package com.govind.ecommerce.service.user;
 
 import com.govind.ecommerce.model.User;
+import com.govind.ecommerce.request.CreateUserRequest;
+import com.govind.ecommerce.request.UpdateUserRequest;
 
 public interface IUserService {
 
     User getUserById(Long userId);
+
+    User createUser(CreateUserRequest request);
+
+    User updateUser(UpdateUserRequest request, Long userId);
+
+    void deleteUser(Long userId);
 
 }
--- src/main/java/com/govind/ecommerce/service/user/UserService.java
+++ src/main/java/com/govind/ecommerce/service/user/UserService.java
@@ -1,14 +1,19 @@
 package com.govind.ecommerce.service.user;
 
+import com.govind.ecommerce.exception.AlreadyExistsException;
 import com.govind.ecommerce.exception.ResourceNotFoundException;
 import com.govind.ecommerce.model.User;
 import com.govind.ecommerce.repository.UserRepository;
+import com.govind.ecommerce.request.CreateUserRequest;
+import com.govind.ecommerce.request.UpdateUserRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class UserService implements IUserService{
+public class UserService implements IUserService {
 
     private final UserRepository userRepository;
 
@@ -16,5 +21,41 @@ public class UserService implements IUserService{
     public User getUserById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    @Override
+    public User createUser(CreateUserRequest request) {
+        return Optional.of(request)
+                .filter(user -> !userRepository.existsByEmail(request.getEmail())) // if the user not exist
+                .map(req -> {
+                    User user = new User();
+                    user.setFirstName(request.getFirstName());
+                    user.setLastName(req.getLastName());
+                    user.setEmail(req.getEmail());
+                    user.setPassword(req.getPassword());
+                    return userRepository.save(user);
+                })
+                .orElseThrow(() -> new AlreadyExistsException("The email: " + request.getEmail() + " already exists"));
+    }
+
+    @Override
+    public User updateUser(UpdateUserRequest request, Long userId) {
+        return userRepository.findById(userId)
+                .map(existingUser -> {
+                    if (request.getFirstName() != null)
+                        existingUser.setFirstName(request.getFirstName());
+                    if (request.getLastName() != null)
+                        existingUser.setLastName(request.getLastName());
+                    return userRepository.save(existingUser);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.findById(userId)
+                .ifPresentOrElse(userRepository::delete, () -> {
+                    throw new ResourceNotFoundException("User not found");
+                });
     }
 }
